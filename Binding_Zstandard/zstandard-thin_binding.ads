33ab612d4fd2bd7c197cb5c9b74e5ab8fd32241d
jrmarino <draco@marino.st>
2016-07-31 12:27:14 -0500

    Handle incompletely types differently
    All we wanted was the Access to the context structures, not the
    structures themselves (which are sure to change often).  Switch these to
    System Addresses and eliminate the complexity.

--- Binding_Zstandard/zstandard-thin_binding.ads
+++ Binding_Zstandard/zstandard-thin_binding.ads
@@ -9,12 +9,6 @@ package Zstandard.Thin_Binding is
    package IC  renames Interfaces.C;
    package ICS renames Interfaces.C.Strings;
 
-   -----------------
-   --  Constants  --
-   -----------------
-
-   ZSTD_REP_NUM : constant IC.int := 3;
-
    ------------------
    --  Data Types  --
    ------------------
@@ -22,92 +16,10 @@ package Zstandard.Thin_Binding is
    type Zstd_uint64 is mod 2 ** 64;
    type Zstd_uint32 is mod 2 ** 32;
 
-   type Zstd_block32 is array (Natural range <>) of Zstd_uint32;
-   type Zstd_block64 is array (Natural range <>) of Zstd_uint64;
-
-   type Zstd_uint32_Access is access all Zstd_uint32;
-
-   type ZSTD_strategy is
-     (ZSTD_fast,
-      ZSTD_dfast,
-      ZSTD_greedy,
-      ZSTD_lazy,
-      ZSTD_lazy2,
-      ZSTD_btlazy2,
-      ZSTD_btopt);
-   pragma Convention (C, ZSTD_strategy);
-
-   type ZSTD_compressionParameters is record
-      windowLog    : IC.unsigned;
-      chainLog     : IC.unsigned;
-      hashLog      : IC.unsigned;
-      searchLog    : IC.unsigned;
-      searchLength : IC.unsigned;
-      targetLength : IC.unsigned;
-      strategy     : ZSTD_strategy;
-   end record;
-   pragma Convention (C, ZSTD_compressionParameters);
-
-   type ZSTD_frameParameters is record
-      contentSizeFlag : IC.unsigned;
-      checksumFlag    : IC.unsigned;
-      noDictIDFlag    : IC.unsigned;
-   end record;
-   pragma Convention (C, ZSTD_frameParameters);
-
-   type ZSTD_parameters is record
-      cParams : ZSTD_compressionParameters;
-      fParams : ZSTD_frameParameters;
-   end record;
-   pragma Convention (C, ZSTD_parameters);
-
-   type XXH64_state_t is record
-      total_len : Zstd_uint64;
-      seed      : Zstd_uint64;
-      v1        : Zstd_uint64;
-      v2        : Zstd_uint64;
-      v3        : Zstd_uint64;
-      v4        : Zstd_uint64;
-      mem64     : Zstd_block64 (1 .. 4);
-      memsize   : IC.unsigned;
-   end record;
-   pragma Convention (C, XXH64_state_t);
-
-   type ZSTD_customMem is record
-      customAlloc : System.Address;  --  void* (*ZSTD_allocFunction) (void* opaque, size_t size);
-      customFree  : System.Address;  --  void  (*ZSTD_freeFunction) (void* opaque, void* address);
-      opaque      : System.Address;
-   end record;
-   pragma Convention (C, ZSTD_customMem);
-
-   type ZSTD_CCtx is record
-      nextSrc          : ICS.chars_ptr;
-      base             : ICS.chars_ptr;
-      dictBase         : ICS.chars_ptr;
-      dictLimit        : Zstd_uint32;
-      lowLimit         : Zstd_uint32;
-      nextToUpdate     : Zstd_uint32;
-      nextToUpdate3    : Zstd_uint32;
-      hashLog3         : Zstd_uint32;
-      loadedDictEnd    : Zstd_uint32;
-      stage            : Zstd_uint32;
-      rep              : Zstd_block32 (1 .. ZSTD_REP_NUM);
-      savedRep         : Zstd_block32 (1 .. ZSTD_REP_NUM);
-      dictID           : Zstd_uint32;
-      params           : ZSTD_parameters;
-      workspace        : System.Address;
-      workSpaceSize    : IC.size_t;
-      blockSize        : IC.size_t;
-      frameContentSize : Zstd_uint64;
-      xxhState         : XXH64_state_t;
-      customMem        : ZSTD_customMem;
-      seqStore         :
-      hashTable        : Zstd_uint32_Access;
-      hashTable3       : Zstd_uint32_Access;
-      chainTable       : Zstd_uint32_Access;
-   end record;
-
-
+   type ZSTD_CCtx_ptr  is new System.Address;
+   type ZSTD_DCtx_ptr  is new System.Address;
+   type ZSTD_CDict_ptr is new System.Address;
+   type ZSTD_DDict_ptr is new System.Address;
 
    ---------------
    --  Version  --
@@ -121,15 +33,15 @@ package Zstandard.Thin_Binding is
    ------------------------
 
    --  tells if a `size_t` function result is an error code
-   function ZSTD_isError (IC.size_t code) return IC.unsigned;
+   function ZSTD_isError (code : IC.size_t) return IC.unsigned;
    pragma Import (C, ZSTD_isError, "ZSTD_isError");
 
    --  provides readable string for an error code
-   function ZSTD_getErrorName (IC.size_t code) return ICS.chars_ptr;
+   function ZSTD_getErrorName (code : IC.size_t) return ICS.chars_ptr;
    pragma Import (C, ZSTD_getErrorName, "ZSTD_getErrorName");
 
    --  maximum compressed size (worst case scenario)
-   function ZSTD_compressBound (IC.size_t srcSize) return IC.size_t;
+   function ZSTD_compressBound (srcSize : IC.size_t) return IC.size_t;
    pragma Import (C, ZSTD_compressBound, "ZSTD_compressBound");
 
    ------------------------
@@ -173,7 +85,7 @@ package Zstandard.Thin_Binding is
    --  Note 2 : `dict` must remain accessible and unmodified during compression operation.
    --  Note 3 : `dict` can be `NULL`, in which case, it's equivalent to ZSTD_compressCCtx() */
    function ZSTD_compress_usingDict
-     (ctx  : ZSTD_CCtx_Access;
+     (ctx  : ZSTD_CCtx_ptr;
       dst  : ICS.chars_ptr; dstCapacity : IC.size_t;
       src  : ICS.chars_ptr; srcSize     : IC.size_t;
       dict : ICS.chars_ptr; dictSize    : IC.size_t;
@@ -186,10 +98,51 @@ package Zstandard.Thin_Binding is
    --  Note 2 : `dict` must remain accessible and unmodified during compression operation.
    --  Note 3 : `dict` can be `NULL`, in which case, it's equivalent to ZSTD_decompressDCtx()
    function ZSTD_decompress_usingDict
-     (dctx : ZSTD_DCtx_Access;
+     (dctx : ZSTD_DCtx_ptr;
       dst  : ICS.chars_ptr; dstCapacity : IC.size_t;
       src  : ICS.chars_ptr; srcSize     : IC.size_t;
       dict : ICS.chars_ptr; dictSize    : IC.size_t) return IC.size_t;
    pragma Import (C, ZSTD_decompress_usingDict, "ZSTD_decompress_usingDict");
+
+   -------------------------------
+   --  Advanced Dictionary API  --
+   -------------------------------
+
+   --  Create a digested dictionary, ready to start compression operation without startup delay.
+   --  `dict` can be released after creation
+   function ZSTD_createCDict
+     (dict : ICS.chars_ptr; dictSize : IC.size_t;
+      compressionLevel : IC.int) return ZSTD_CDict_ptr;
+   pragma Import (C, ZSTD_createCDict, "ZSTD_createCDict");
+
+   function ZSTD_freeCDict (CDict : ZSTD_CDict_ptr) return IC.size_t;
+   pragma Import (C, ZSTD_freeCDict, "ZSTD_freeCDict");
+
+   --  Compression using a pre-digested Dictionary.
+   --  Much faster than ZSTD_compress_usingDict() when same dictionary is used multiple times.
+   --  Note that compression level is decided during dictionary creation
+   function ZSTD_compress_usingCDict
+     (ctx   : ZSTD_CCtx_ptr;
+      dst   : ICS.chars_ptr; dstCapacity : IC.size_t;
+      src   : ICS.chars_ptr; srcSize     : IC.size_t;
+      CDict : ZSTD_CDict_ptr) return IC.size_t;
+   pragma Import (C, ZSTD_compress_usingCDict, "ZSTD_compress_usingCDict");
+
+   --  Create a digested dictionary, ready to start decompression operation without startup delay.
+   --  `dict` can be released after creation
+   function ZSTD_createDDict (dict : ICS.chars_ptr; dictSize : IC.size_t) return ZSTD_DDict_ptr;
+   pragma Import (C, ZSTD_createDDict, "ZSTD_createDDict");
+
+   function ZSTD_freeDDict (ddict : ZSTD_DDict_ptr) return IC.size_t;
+   pragma Import (C, ZSTD_freeDDict, "ZSTD_freeDDict");
+
+   --   Decompression using a pre-digested Dictionary
+   --  Much faster than ZSTD_decompress_usingDict() when same dictionary is used multiple times.
+   function ZSTD_decompress_usingDDict
+     (dctx  : ZSTD_DCtx_ptr;
+      dst   : ICS.chars_ptr; dstCapacity : IC.size_t;
+      src   : ICS.chars_ptr; srcSize     : IC.size_t;
+      ddict : ZSTD_DDict_ptr) return IC.size_t;
+   pragma Import (C, ZSTD_decompress_usingDDict, "ZSTD_decompress_usingDDict");
 
 end Zstandard.Thin_Binding;
